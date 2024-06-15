3fe3dac5b2d85efedd9213423f994cfe9770c415
amimanye <amimanyeaglago@gmail.com>
2024-06-15 15:31:07 +0000

    action formspree, about to test

--- scripts/contactForm.js
+++ scripts/contactForm.js
@@ -16,6 +16,32 @@ submitButton.addEventListener("click", (e) => {
     const message = document.querySelector("textarea[name=message]").value
 
     // sending the information to the server
-
-    
+    // sendToServer(firstName, lastName, email, message);
 })
+
+const sendToServer = (firstName, lastName, email, message) => {
+    const form = document.querySelector("form")
+    form.addEventListener("submit", async (e) => { 
+        e.preventDefault();
+        const data = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            message: message
+        }
+        const response = await fetch("https://formspree.io/f/mlelmple", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        const result = await response.json();
+        console.log(result);
+        if (result.ok) {
+            form.reset();
+        } else {
+            console.log("error");
+        }
+    })
+}
